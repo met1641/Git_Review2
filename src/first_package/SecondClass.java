42c06e25fb9a39e18c126c8218816e024314eda8
msivass <mehmetsivass@gmail.com>
2023-01-07 14:33:15 -0500

    Initial Commit

--- src/first_package/SecondClass.java
+++ src/first_package/SecondClass.java
@@ -1,8 +1,9 @@
 package first_package;
 
 public class SecondClass {
-  
-   public static void main(String[] args) {
+
+    public static void main(String[] args) {
 
         System.out.println("First class");
+    }
 }
